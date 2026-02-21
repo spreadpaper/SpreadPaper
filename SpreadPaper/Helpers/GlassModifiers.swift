7c0d9ef992df83700c8d0d567f4b5352354e251a
Robin van Baalen <robin@ganttify.com>
2026-02-21 11:53:27 -0400

    fix: wrap macOS 26 glassEffect API in compiler check for Xcode 16 CI

--- SpreadPaper/Helpers/GlassModifiers.swift
+++ SpreadPaper/Helpers/GlassModifiers.swift
@@ -3,10 +3,14 @@ import SwiftUI
 extension View {
     @ViewBuilder
     func adaptiveGlass<S: Shape>(in shape: S) -> some View {
+        #if compiler(>=6.2)
         if #available(macOS 26, *) {
             self.glassEffect(.regular, in: shape)
         } else {
             self.background(.ultraThinMaterial, in: shape)
         }
+        #else
+        self.background(.ultraThinMaterial, in: shape)
+        #endif
     }
 }
