9c98a900d4ec41c558772b0f8c16d73dc8ba56cf
Robin van Baalen <robin@ganttify.com>
2026-04-08 18:08:29 -0400

    chore: remove old SwiftUI views replaced by Cool Dark redesign
    Removed: ContentView, SidebarView, DynamicEditorView, TimelineView,
    ImageDropZone, CanvasView, MonitorOverlayView, GlassModifiers,
    WindowDragHandler, WindowAccessor.

--- SpreadPaper/Helpers/WindowDragHandler.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-import AppKit
-
-struct WindowDragHandler: NSViewRepresentable {
-    func makeNSView(context: Context) -> NSView { DraggableView() }
-    func updateNSView(_ nsView: NSView, context: Context) {}
-    class DraggableView: NSView { override var mouseDownCanMoveWindow: Bool { true } }
-}
